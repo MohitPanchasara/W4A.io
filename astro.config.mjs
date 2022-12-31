2063026feebde69ab3768f34d9c9b3dd1fffcb1d
Mohit Panchasara <mohitpanchasara@gmail.com>
2022-12-31 19:51:29 +0530

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
+  server: { port: 2000, host: true },
   integrations: [
     tailwind(),
+    react(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,36 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config}*/
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      satoshi: ["Satoshi Variable"],
       fontFamily: {
-        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+        grotesk: ["Space Grotesk"],
       },
       colors: {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
+        primaryweb: "#0A090F",
+        secodaryweb: "#232126",
+        tertiaryweb: "#3D3D40",
+        transparent: "transparent",
+        current: "currentColor",
+        mainBlue: "#3F00F9",
+        mainBlack: "#18121F",
+        compWhite: "#F5DFD8",
+        accent: "#15121F",
+        webSky: "#0894FF",
+        webSkyLite: "#C4E5FF",
+        webBlue: "#6E53FF",
+        webBlueLite: "#DCD6FF",
+        webPurpleLite: "#ECCFFA",
+        webPink: "#F95088",
+        webPinkLite: "#FED5E3",
+        webPeach: "#FF6F62",
+        webPeachLite: "#FFDDD9",
       },
       textColor: {
         default: "var(--color-text)",
@@ -27,5 +48,5 @@ module.exports = {
   corePlugins: {
     fontSize: false,
   },
-  plugins: [require("tailwindcss-fluid-type")],
+  plugins: [require("tailwindcss-fluid-type"), require("@tailwindcss/forms")],
 };
