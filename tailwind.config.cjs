6478283d6b32181bd09e1b4541f656d104d17d7a
DevangDelvadiya11 <120778772+DevangDelvadiya11@users.noreply.github.com>
2023-02-01 18:53:50 +0530

    Updated Packages

---
+++ tailwind.config.cjs
@@ -0,0 +1,59 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config}*/
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: { max: "480px" },
+      md: { min: "481px", max: "769px" },
+      lg: { min: "770px" },
+    },
+
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi-Variable", "sans-serif"],
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+        grotesk: ["Space Grotesk"],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        primaryweb: "#0A090F",
+        secodaryweb: "#232126",
+        tertiaryweb: "#3D3D40",
+        transparent: "transparent",
+        current: "currentColor",
+        mainBlue: "#3F00F9",
+        mainBlack: "#18121F",
+        compWhite: "#F5DFD8",
+        accent: "#15121F",
+        webSky: "#0894FF",
+        webSkyLite: "#C4E5FF",
+        webBlue: "#6E53FF",
+        webBlueLite: "#DCD6FF",
+        webPurpleLite: "#ECCFFA",
+        webPink: "#F95088",
+        webPinkLite: "#FED5E3",
+        webPeach: "#FF6F62",
+        webPeachLite: "#FFDDD9",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type"), require("@tailwindcss/forms")],
+};
