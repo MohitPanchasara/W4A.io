da996698268e95afa35228755ddb12fc5401410f
Mohit Panchasara <mohit.p@admybrand.com>
2023-02-05 22:33:43 +0530

    Deploy to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,23 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config V
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  server: { port: 2000, host: true },
-  integrations: [
-    tailwind(),
-    react(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  server: {
+    port: 3000,
+    host: true
+  },
+  integrations: [tailwind(), react(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
