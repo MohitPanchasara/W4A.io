7b6eaddc357f07241af3837382e880eded211cf7
devangdelvadiya <201951055@iiitvadodara.ac.in>
2023-04-04 23:16:55 +0530

    i18n implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import astroI18next from "astro-i18next";
 // https://astro.build/config V
 export default defineConfig({
   site: "https://w4a.netlify.app/",
   server: { port: 2000, host: true },
   integrations: [
+    astroI18next(),
     tailwind(),
     react(),
     image({
